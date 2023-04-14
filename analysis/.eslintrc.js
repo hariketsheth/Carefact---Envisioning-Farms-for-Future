cfa878668824181e802902c2823bc93aac57a49e
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@vitstudent.ac.in>
2023-04-14 12:06:00 +0530

    ✨ Initialised Project

---
+++ analysis/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0
+  },
+};
